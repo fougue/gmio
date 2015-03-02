1518324fba1095ff5faf7ff5331552c8dc7f131d
Hugues Delorme <delorme.hugues@fougsys.fr>
2015-03-02 17:38:33 +0100

    Add licence files and headers

--- src/gmio_core/memory.h
+++ src/gmio_core/memory.h
@@ -1,3 +1,20 @@
+/****************************************************************************
+**
+** GeomIO Library
+** Copyright FougSys (2 Mar. 2015)
+** contact@fougsys.fr
+**
+** This software is a reusable library whose purpose is to provide complete
+** I/O support for various CAD file formats (eg. STL)
+**
+** This software is governed by the CeCILL-B license under French law and
+** abiding by the rules of distribution of free software.  You can  use,
+** modify and/ or redistribute the software under the terms of the CeCILL-B
+** license as circulated by CEA, CNRS and INRIA at the following URL
+** "http://www.cecill.info".
+**
+****************************************************************************/
+
 #ifndef GMIO_MEMORY_H
 #define GMIO_MEMORY_H
 
