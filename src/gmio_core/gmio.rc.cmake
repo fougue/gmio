ac0c7e2215c1fa88ae89be9e4d2f369586a5f96f
Hugues Delorme <delorme.hugues@fougsys.fr>
2015-05-27 18:57:00 +0200

    cmake: add generation of version.h and gmio.rc (DLL information)

---
+++ src/gmio_core/gmio.rc.cmake
@@ -0,0 +1,25 @@
+#include <winresrc.h>
+
+VS_VERSION_INFO VERSIONINFO
+ FILEVERSION    @GMIO_VERSION_MAJOR@,@GMIO_VERSION_MINOR@,@GMIO_VERSION_PATCH@,0
+ PRODUCTVERSION @GMIO_VERSION_MAJOR@,@GMIO_VERSION_MINOR@,@GMIO_VERSION_PATCH@,0
+ FILETYPE VFT_DLL
+ BEGIN
+   BLOCK "StringFileInfo"
+   BEGIN
+     BLOCK "04090000"
+     BEGIN
+       VALUE "FileDescription", "GeomIO DLL"
+       VALUE "FileVersion", "@GMIO_VERSION@"
+       VALUE "InternalName", "gmio"
+       VALUE "LegalCopyright", "Licensed under CeCILL-B v1.0"
+       VALUE "ProductName", "GeomIO library, by Fougue Ltd."
+       VALUE "ProductVersion", "@GMIO_VERSION@"
+     END
+   END
+   BLOCK "VarFileInfo"
+   BEGIN
+     VALUE "Translation", 0x409, 1200
+   END
+ END
+ 
