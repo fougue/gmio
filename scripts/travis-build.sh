21491bbf6b54aaeda8b089334842968c69d699fd
Hugues Delorme <delorme.hugues@fougue.pro>
2017-03-16 16:42:42 +0100

    travis: use codecov instead of coveralls

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -3,22 +3,14 @@
 # Make the script fails on any command error
 set -e
 
-# Install cpp-coveralls only if OS==linux and CC==gcc
-if [ "${TRAVIS_COVERALLS}" = "ON" ]      \
-    && [ "${TRAVIS_OS_NAME}" = "linux" ] \
-    && [ "${CC}" = "gcc" ];
-then
-    export PYTHONUSERBASE=`pwd`/pip;
-    pip install --user cpp-coveralls;
-else
-    export TRAVIS_COVERALLS=OFF;
-fi
-echo TRAVIS_COVERALLS=$TRAVIS_COVERALLS
-
 # CMake options as environment variables
 if [ -n "${TRAVIS_GCC_VERSION}" ]; then
     export CC=gcc-$TRAVIS_GCC_VERSION;
     export CXX=g++-$TRAVIS_GCC_VERSION;
+fi
+
+if [ -z "${TRAVIS_TESTS_COVERAGE}" ]; then
+    export TRAVIS_TESTS_COVERAGE=OFF;
 fi
 
 if [ -z "${TRAVIS_FLOAT2STR_LIB}" ]; then
@@ -38,22 +30,10 @@ cmake .. -G "Unix Makefiles"       \
          -DGMIO_BUILD_BENCHMARK_ASSIMP=OFF             \
          -DGMIO_BUILD_BENCHMARK_OPENCASCADE=OFF        \
          -DGMIO_BUILD_TESTS_FAKE_SUPPORT=ON            \
-         -DGMIO_BUILD_TESTS_COVERAGE=$TRAVIS_COVERALLS \
+         -DGMIO_BUILD_TESTS_COVERAGE=$TRAVIS_TESTS_COVERAGE \
          -DGMIO_FLOAT2STR_LIB=$TRAVIS_FLOAT2STR_LIB
 
 # Make
 make -j4 VERBOSE=1
 make install
 make check  # Unit tests
-
-# Coveralls
-if [ "${TRAVIS_COVERALLS}" = "ON" ]; then
-    $PYTHONUSERBASE/bin/coveralls   \
-        --exclude benchmarks        \
-        --exclude build/CMakeFiles  \
-        --exclude examples          \
-        --exclude install           \
-        --exclude tests             \
-        --gcov-options '\-lp'       \
-        --root .. --build-root .    ;
-fi
