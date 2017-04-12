e8129237b115874cc4b93ce87703ebd1760a3eab
Hugues Delorme <delorme.hugues@fougue.pro>
2017-04-12 10:28:11 +0200

    benchmark_gmio: add benchmarks for AMF

--- benchmarks/benchmark_gmio/main.c
+++ benchmarks/benchmark_gmio/main.c
@@ -28,6 +28,7 @@
 ****************************************************************************/
 
 #include <gmio_core/error.h>
+#include <gmio_amf/amf_io.h>
 #include <gmio_stl/stl_io.h>
 #include <gmio_stl/stl_io_options.h>
 #include <gmio_stl/stl_mesh.h>
@@ -38,6 +39,7 @@
 #include "../commons/benchmark_tools.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct gmio_stl_triangle;
@@ -86,147 +88,224 @@ static enum gmio_endianness to_byte_order(enum gmio_stl_format format)
     return GMIO_ENDIANNESS_UNKNOWN;
 }
 
-enum { STL_TRIANGLE_ARRAY_SIZE = 512 };
-struct stl_readwrite_conv
-{
-    struct gmio_stream stream;
-    struct gmio_streampos out_stream_pos_begin;
-    enum gmio_stl_format in_format;
-    enum gmio_stl_format out_format;
-    struct gmio_stl_triangle triangle_array[STL_TRIANGLE_ARRAY_SIZE];
-    unsigned triangle_pos;
-    uint32_t total_triangle_count;
+enum { GMIO_OUTPUT_FORMAT_TAG_AMF = 0x1000 };
+enum gmio_output_format {
+    GMIO_OUTPUT_FORMAT_STL_ASCII = GMIO_STL_FORMAT_ASCII,
+    GMIO_OUTPUT_FORMAT_STL_BINARY_LE = GMIO_STL_FORMAT_BINARY_LE,
+    GMIO_OUTPUT_FORMAT_STL_BINARY_BE = GMIO_STL_FORMAT_BINARY_BE,
+    GMIO_OUTPUT_FORMAT_AMF_PLAIN = GMIO_OUTPUT_FORMAT_TAG_AMF + 1,
+    GMIO_OUTPUT_FORMAT_AMF_ZIP
 };
 
-static void readwrite_begin_solid(
-        void* cookie, const struct gmio_stl_mesh_creator_infos* infos)
-{
-    struct stl_readwrite_conv* rw_conv = (struct stl_readwrite_conv*)cookie;
-    struct gmio_stream* stream = &rw_conv->stream;
+struct bmk_gmio_stl_convert_params {
+    const char* input_filepath;
+    const char* output_filepath;
+    enum gmio_output_format output_format;
+};
 
-    if (infos->format == GMIO_STL_FORMAT_ASCII) {
-        if (rw_conv->out_format == GMIO_STL_FORMAT_ASCII) {
-            stream->func_write(stream->cookie, "solid ", 1, 6);
-            stream->func_write(
-                        stream->cookie,
-                        infos->stla_solid_name,
-                        sizeof(char),
-                        strlen(infos->stla_solid_name));
-            stream->func_write(stream->cookie, "\n", 1, 1);
-        }
-        else {
-            /* For binary STL, facet count <- 0 because it cannot be known at this
-             * point. Header will be correctly rewritten at the end of the read
-             * procedure (in gmio_stl_mesh_creator::func_end_solid() callback)
-             */
-            const enum gmio_endianness byte_order = to_byte_order(rw_conv->out_format);
-            gmio_stlb_header_write(stream, byte_order, NULL, 0);
-        }
-    }
-    else {
-        if (rw_conv->out_format == GMIO_STL_FORMAT_ASCII) {
-            stream->func_write(stream->cookie, "solid\n", 1, 6);
-        }
-        else {
-            gmio_stlb_header_write(
-                        stream,
-                        to_byte_order(rw_conv->out_format),
-                        infos->stlb_header,
-                        infos->stlb_triangle_count);
-        }
-    }
-}
+struct gmio_stl_data {
+    struct gmio_stl_triangle* vec_triangle;
+    uint32_t vec_triangle_count;
+};
 
-static void readwrite_get_triangle(
+static void stldata_get_triangle(
         const void* cookie, uint32_t tri_id, struct gmio_stl_triangle* triangle)
 {
-    const struct gmio_stl_triangle* tri_array =
-            (const struct gmio_stl_triangle*)cookie;
-    *triangle = tri_array[tri_id];
+    const struct gmio_stl_data* stldata = (const struct gmio_stl_data*)cookie;
+    *triangle = stldata->vec_triangle[tri_id];
 }
 
-static void stl_readwrite_flush_triangles(struct stl_readwrite_conv* rw_conv)
-{
-    struct gmio_stl_mesh mesh = {0};
-    struct gmio_stl_write_options options = {0};
-
-    mesh.cookie = &rw_conv->triangle_array[0];
-    mesh.triangle_count = rw_conv->triangle_pos;
-    mesh.func_get_triangle = &readwrite_get_triangle;
-
-    options.stl_write_triangles_only = true;
-    options.stla_float32_format = GMIO_FLOAT_TEXT_FORMAT_SCIENTIFIC_LOWERCASE;
-    options.stla_float32_prec = 6;
-
-    gmio_stl_write(rw_conv->out_format, &rw_conv->stream, &mesh, &options);
-    rw_conv->triangle_pos = 0;
-}
-
-static void readwrite_add_triangle(
+static void stldata_add_triangle(
         void* cookie, uint32_t tri_id, const struct gmio_stl_triangle* triangle)
 {
-    struct stl_readwrite_conv* rw_conv = (struct stl_readwrite_conv*)cookie;
-    if (rw_conv->triangle_pos >= STL_TRIANGLE_ARRAY_SIZE)
-        stl_readwrite_flush_triangles(rw_conv);
-    rw_conv->triangle_array[rw_conv->triangle_pos] = *triangle;
-    ++(rw_conv->triangle_pos);
-    rw_conv->total_triangle_count = tri_id + 1;
+    struct gmio_stl_data* stldata = (struct gmio_stl_data*)cookie;
+    stldata->vec_triangle[tri_id] = *triangle;
 }
 
-static void readwrite_end_solid(void* cookie)
+static void stldata_amf_get_document_element(
+        const void* cookie,
+        enum gmio_amf_document_element element,
+        uint32_t element_index,
+        void* ptr_element)
 {
-    struct stl_readwrite_conv* rw_conv = (struct stl_readwrite_conv*)cookie;
-    struct gmio_stream* stream = &rw_conv->stream;
-    if (rw_conv->triangle_pos != 0)
-        stl_readwrite_flush_triangles(rw_conv);
-    if (rw_conv->out_format == GMIO_STL_FORMAT_ASCII) {
-        stream->func_write(stream->cookie, "endsolid", 1, 8);
-    }
-    else if (rw_conv->in_format == GMIO_STL_FORMAT_ASCII) {
-        const enum gmio_endianness byte_order = to_byte_order(rw_conv->out_format);
-        /* The total facet count has to be written because it wasn't known at
-         * the beginning of the read procedure */
-        stream->func_set_pos(stream->cookie, &rw_conv->out_stream_pos_begin);
-        gmio_stlb_header_write(
-                    stream, byte_order, NULL, rw_conv->total_triangle_count);
+    GMIO_UNUSED(cookie);
+    if (element == GMIO_AMF_DOCUMENT_ELEMENT_OBJECT) {
+        struct gmio_amf_object* object = (struct gmio_amf_object*)ptr_element;
+        object->id = element_index;
+        object->mesh_count = 1;
     }
 }
 
-static void bmk_gmio_stl_readwrite_conv(const void* filepath)
+static void stldata_amf_get_object_mesh(
+        const void* cookie,
+        uint32_t object_index,
+        uint32_t mesh_index,
+        struct gmio_amf_mesh* ptr_mesh)
 {
-    FILE* infile = fopen(filepath, "rb");
-    FILE* outfile = fopen("_readwrite_conv.stl", "wb");
-    struct gmio_stream instream = {0};
-    struct stl_readwrite_conv rw_conv = {0};
-    struct gmio_stl_mesh_creator mesh_creator = {0};
-    int error = GMIO_ERROR_OK;
+    GMIO_UNUSED(object_index);
+    GMIO_UNUSED(mesh_index);
+    const struct gmio_stl_data* stldata = (const struct gmio_stl_data*)cookie;
+    ptr_mesh->vertex_count = stldata->vec_triangle_count * 3;
+    ptr_mesh->edge_count = 0;
+    ptr_mesh->volume_count = 1;
+}
 
-    /* rw_conv.out_format = GMIO_STL_FORMAT_BINARY_LE; */
-    rw_conv.out_format = GMIO_STL_FORMAT_ASCII;
-
-    if (infile != NULL) {
-        instream = gmio_stream_stdio(infile);
-        rw_conv.in_format = gmio_stl_format_probe(&instream);
+static void stldata_amf_get_object_mesh_element(
+        const void* cookie,
+        const struct gmio_amf_object_mesh_element_index* element_index,
+        void* ptr_element)
+{
+    const struct gmio_stl_data* stldata = (const struct gmio_stl_data*)cookie;
+    switch (element_index->element_type) {
+    case GMIO_AMF_MESH_ELEMENT_VERTEX: {
+        struct gmio_amf_vertex* dst_vertex = (struct gmio_amf_vertex*)ptr_element;
+        const uint32_t vertex_id = element_index->value;
+        const uint32_t facet_id = vertex_id / 3;
+        const uint32_t facet_vertex_id = vertex_id % 3;
+        const struct gmio_stl_triangle* tri = &stldata->vec_triangle[facet_id];
+        const struct gmio_vec3f* src_vertex = &tri->v1 + facet_vertex_id;
+        dst_vertex->coords.x = src_vertex->x;
+        dst_vertex->coords.y = src_vertex->y;
+        dst_vertex->coords.z = src_vertex->z;
+        break;
     }
-    if (outfile != NULL) {
-        rw_conv.stream = gmio_stream_stdio(outfile);
-        rw_conv.stream.func_get_pos(
-                    rw_conv.stream.cookie,
-                    &rw_conv.out_stream_pos_begin);
+    case GMIO_AMF_MESH_ELEMENT_EDGE:
+        break;
+    case GMIO_AMF_MESH_ELEMENT_VOLUME: {
+        struct gmio_amf_volume* ptr_volume = (struct gmio_amf_volume*)ptr_element;
+        ptr_volume->materialid = 1;
+        ptr_volume->triangle_count = stldata->vec_triangle_count;
+        break;
+    }
+    }
+}
+
+static void stldata_amf_get_object_mesh_volume_triangle(
+        const void* cookie,
+        const struct gmio_amf_object_mesh_element_index* volume_index,
+        uint32_t triangle_index,
+        struct gmio_amf_triangle* ptr_triangle)
+{
+    GMIO_UNUSED(cookie);
+    GMIO_UNUSED(volume_index);
+    const uint32_t base_vertex_id = triangle_index*3;
+    ptr_triangle->v1 = base_vertex_id + 0;
+    ptr_triangle->v2 = base_vertex_id + 1;
+    ptr_triangle->v3 = base_vertex_id + 2;
+}
+
+static void bmk_gmio_stl_convert(const void* gparams)
+{
+    const struct bmk_gmio_stl_convert_params* params =
+            (const struct bmk_gmio_stl_convert_params*)gparams;
+
+    FILE* infile = fopen(params->input_filepath, "rb");
+    FILE* outfile = fopen(params->output_filepath, "wb");
+    struct gmio_stream instream =
+            infile != NULL ? gmio_stream_stdio(infile) : gmio_stream_null();
+    struct gmio_stream outstream =
+            outfile != NULL ? gmio_stream_stdio(outfile) : gmio_stream_null();
+
+    /* Find the count of facet in the input STL file */
+    struct gmio_stl_infos stlinfos = {0};
+    gmio_stl_infos_probe(
+                &stlinfos, &instream, GMIO_STL_INFO_FLAG_FACET_COUNT, NULL);
+    /* printf("Facet count: %u\n", stlinfos.facet_count); */
+
+    /* Create the array of STL triangles */
+    struct gmio_stl_data stldata = {0};
+    stldata.vec_triangle = malloc(
+                sizeof(struct gmio_stl_triangle) * stlinfos.facet_count);
+    stldata.vec_triangle_count = stlinfos.facet_count;
+
+    /* Read the STL data */
+    struct gmio_stl_mesh_creator stlmesh_creator = {0};
+    stlmesh_creator.cookie = &stldata;
+    stlmesh_creator.func_add_triangle = &stldata_add_triangle;
+    int error = gmio_stl_read(&instream, &stlmesh_creator, NULL);
+    if (error != GMIO_ERROR_OK) {
+        fprintf(stderr, "gmio_stl_read() error: 0x%X\n", error);
+        goto label_end;
     }
 
-    mesh_creator.cookie = &rw_conv;
-    mesh_creator.func_begin_solid = &readwrite_begin_solid;
-    mesh_creator.func_add_triangle = &readwrite_add_triangle;
-    mesh_creator.func_end_solid = &readwrite_end_solid;
+    /* Write the STL data */
+    if (params->output_format == GMIO_STL_FORMAT_ASCII
+            || params->output_format & GMIO_STL_FORMAT_TAG_BINARY)
+    {
+        struct gmio_stl_mesh stlmesh = {0};
+        stlmesh.cookie = &stldata;
+        stlmesh.triangle_count = stlinfos.facet_count;
+        stlmesh.func_get_triangle = &stldata_get_triangle;
+        const enum gmio_stl_format stlformat =
+                (enum gmio_stl_format)params->output_format;
+        error = gmio_stl_write(stlformat, &outstream, &stlmesh, NULL);
+    }
+    else if (params->output_format & GMIO_OUTPUT_FORMAT_TAG_AMF) {
+        struct gmio_amf_document doc = {0};
+        doc.cookie = &stldata;
+        doc.unit = GMIO_AMF_UNIT_MILLIMETER;
+        doc.object_count = 1;
+        doc.func_get_document_element = &stldata_amf_get_document_element;
+        doc.func_get_object_mesh = &stldata_amf_get_object_mesh;
+        doc.func_get_object_mesh_element = &stldata_amf_get_object_mesh_element;
+        doc.func_get_object_mesh_volume_triangle =
+                &stldata_amf_get_object_mesh_volume_triangle;
+        struct gmio_amf_write_options options = {0};
+        options.create_zip_archive =
+                params->output_format == GMIO_OUTPUT_FORMAT_AMF_ZIP;
+        error = gmio_amf_write(&outstream, &doc, &options);
+        gmio_memblock_deallocate(&options.stream_memblock);
+    }
 
-    error = gmio_stl_read(&instream, &mesh_creator, NULL);
-
-    if (error != GMIO_ERROR_OK)
-        fprintf(stderr, "gmio error: 0x%X\n", error);
-
+label_end:
     fclose(infile);
     fclose(outfile);
+    free(stldata.vec_triangle);
+}
+
+static void bmk_gmio_stl_convert_ascii(const void* filepath)
+{
+    struct bmk_gmio_stl_convert_params params = {0};
+    params.input_filepath = (const char*)filepath;
+    params.output_filepath = "__converted_ascii.stl";
+    params.output_format = GMIO_STL_FORMAT_ASCII;
+    bmk_gmio_stl_convert(&params);
+}
+
+static void bmk_gmio_stl_convert_binary_le(const void* filepath)
+{
+    struct bmk_gmio_stl_convert_params params = {0};
+    params.input_filepath = (const char*)filepath;
+    params.output_filepath = "__converted_binary-le.stl";
+    params.output_format = GMIO_STL_FORMAT_BINARY_LE;
+    bmk_gmio_stl_convert(&params);
+}
+
+static void bmk_gmio_stl_convert_binary_be(const void* filepath)
+{
+    struct bmk_gmio_stl_convert_params params = {0};
+    params.input_filepath = (const char*)filepath;
+    params.output_filepath = "__converted_binary-be.stl";
+    params.output_format = GMIO_STL_FORMAT_BINARY_BE;
+    bmk_gmio_stl_convert(&params);
+}
+
+static void bmk_gmio_stl_convert_amf_plain(const void* filepath)
+{
+    struct bmk_gmio_stl_convert_params params = {0};
+    params.input_filepath = (const char*)filepath;
+    params.output_filepath = "__converted_plain.amf";
+    params.output_format = GMIO_OUTPUT_FORMAT_AMF_PLAIN;
+    bmk_gmio_stl_convert(&params);
+}
+
+static void bmk_gmio_stl_convert_amf_zip(const void* filepath)
+{
+    struct bmk_gmio_stl_convert_params params = {0};
+    params.input_filepath = (const char*)filepath;
+    params.output_filepath = "__converted_amf.zip";
+    params.output_format = GMIO_OUTPUT_FORMAT_AMF_ZIP;
+    bmk_gmio_stl_convert(&params);
 }
 
 void bmk_gmio_stl_infos_probe_all(const void* filepath)
@@ -279,8 +358,20 @@ int main(int argc, char** argv)
             { "read_file()",
               bmk_gmio_stl_read, NULL,
               NULL, NULL },
-            { "readwrite_conv()",
-              bmk_gmio_stl_readwrite_conv, NULL,
+            { "conv_stla()",
+              bmk_gmio_stl_convert_ascii, NULL,
+              NULL, NULL },
+            { "conv_stlb_le()",
+              bmk_gmio_stl_convert_binary_le, NULL,
+              NULL, NULL },
+            { "conv_stlb_be()",
+              bmk_gmio_stl_convert_binary_be, NULL,
+              NULL, NULL },
+            { "conv_amf_plain()",
+              bmk_gmio_stl_convert_amf_plain, NULL,
+              NULL, NULL },
+            { "conv_amf_zip()",
+              bmk_gmio_stl_convert_amf_zip, NULL,
               NULL, NULL },
             { "stl_infos_probe(ALL)",
               bmk_gmio_stl_infos_probe_all, NULL,
@@ -291,15 +382,12 @@ int main(int argc, char** argv)
             {0}
         };
         const size_t cmp_count = GMIO_ARRAY_SIZE(cmp_args) - 1;
+        for (size_t i = 0; i < cmp_count; ++i)
+            cmp_args[i].func1_arg = filepath;
+
         struct benchmark_cmp_result cmp_res[GMIO_ARRAY_SIZE(cmp_args)] = {0};
         struct benchmark_cmp_result_array res_array = {0};
         const struct benchmark_cmp_result_header header = { "gmio", NULL };
-
-        {
-            size_t i = 0;
-            for (i = 0; i < cmp_count; ++i)
-                cmp_args[i].func1_arg = filepath;
-        }
 
         res_array.ptr = &cmp_res[0];
         res_array.count = cmp_count;
